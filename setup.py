6a530dbfca8630dca4bfb56ec4d98a1f448a0d69
Higor Augusto <higoramp@dri.cefetmg.br>
2018-03-05 15:53:07 -0300

    refs #1 corrigindo link imagem do banner do cefetmg

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='oauth_client',
-    version='0.0.10',
+    version='0.0.12',
     packages=['oauth_client'],
     package_data={'oauth_client': ['templates/*.html', 'static/css/*.css', 'static/images/*.*', 'static/fonts/*.*']},
     url='',
